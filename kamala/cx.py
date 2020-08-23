c9c7df3c26f952f288ebb16ba595e97b73f216c3
Dmitry Mitrofanov <dmitry.mitrofanov@nordigy.ru>
2020-08-23 16:06:59 +0300

    ACL works, others are not

--- kamala/cx.py
+++ kamala/cx.py
@@ -27,39 +27,27 @@ API_CLDT_LINK = CX_ENDPOINT + "list/a/{}/u/{}/t/{}/m/clients/date/"
 API_GA_NAME = 'GA'
 API_GA_LINK = CX_ENDPOINT + "getAppointment/a/{}/u/{}/t/{}/id/"
 
+API_USR_NAME = 'USR'
+API_USR_LINK = CX_ENDPOINT + "list/a/{}/u/{}/t/{}/m/Users/?offset="
+
+API_CLT_NAME = 'CLT'
+API_CLT_LINK = CX_ENDPOINT + "list/a/{}/u/{}/t/{}/m/clients/?offset="
+
 API = {API_ACL_NAME: API_ACL_LINK,
        API_REACL_NAME: API_REACL_LINK,
        API_CLDT_NAME: API_CLDT_LINK,
-       API_GA_NAME: API_GA_NAME}
+       API_GA_NAME: API_GA_NAME,
+       API_USR_NAME: API_USR_LINK,
+       API_CLT_NAME: API_CLT_LINK}
 
 
 
 def get_api_link(api_name, page):
-    return API[api_name].format(API_ACCOUNT, API_USER, API_TOKEN) + page
+    return API[api_name].format(API_ACCOUNT, API_USER, API_TOKEN) + str(page)
 
 
 def get_data_chunk(api_name, offset=""):
     api_link = get_api_link(api_name, str(offset))
-    logging.debug(f'Requesting data chunk {api_link}')
+    logging.info(f'Requesting data chunk {api_link}')
     r = requests.get(url=(api_link))
     return r.text
-
-
-def get_pages(api_name, offset=""):
-    if api_name == API_ACL_NAME:
-        l_offset = 0 if offset == "" else int(offset)
-        logging.info(f'Get pages {API_ACL_NAME} offset {l_offset}')
-        r = get_data_chunk(api_name, l_offset)
-        total_count = int(r["total_count"]) - l_offset
-        count = int(r["count"])
-        pages = math.ceil(total_count / count)
-        logging.info(f'Original total count {r["total_count"]}, altered total count {total_count}, count {count}, pages {pages}')
-    #elif api_name == 
-        #return map(lambda x: l_offset + count * --x, range(pages))
-
-
-def get_chunks(api_name, offset=""):
-    return map(lambda x: get_data_chunk(api_name, x), get_pages(api_name, offset))
-
-#def push_acl():
-    #r = get_data_chunk(api_name, l_offset)
--- kamala/sf.py
+++ kamala/sf.py
@@ -15,18 +15,30 @@ def connect():
     return ctx
 
 
-def get_table_name(table_name, prefix="KAMALA_"):
-    return prefix + table_name
+def get_table_name(api_name, prefix="KAMALA_"):
+    return prefix + api_name
 
 
-def mrg_to_table(cs, table_name, data):
-    l_sql = "merge into {} t using (select parse_json('{}') v, current_timestamp ts) s on s.v = parse_json(t.v) when not matched then insert (t.v, changed_at) values (s.v, s.ts)"
-    l_sql = l_sql.format(get_table_name(table_name), data.replace("'", r"\u0027"))
+def create_table(cs, api_name):
+    l_sql = f"create table {get_table_name(api_name)} (v variant, inserted_at timestamp_ntz, etl_id VARCHAR(50))"
+    cs.execute(l_sql)
+
+
+def drop_table(cs, api_name):
+    l_sql = f"drop table if exists {get_table_name(api_name)}"
+    cs.execute(l_sql)
+
+
+def merge_to_table(cs, api_name, data, etl_id):
+    l_sql = "merge into {} t using (select parse_json('{}') v, current_timestamp ts, '{}' etl_id) s \
+             on s.v = parse_json(t.v) when not matched then insert (t.v, t.inserted_at, t.etl_id) values (s.v, s.ts, s.etl_id)"
+    #l_sql = l_sql.format(get_table_name(api_name), data.replace("'", r"\u0027"), etl_id)
+    l_sql = l_sql.format(get_table_name(api_name), data, etl_id)
     logging.debug(l_sql)
     cs.execute(l_sql)
 
 
-def del_from_table(cs, table_name):
+def delele_from_table(cs, api_name):
     l_sql = "delete from {}"
-    l_sql = l_sql.format(get_table_name(table_name))
+    l_sql = l_sql.format(get_table_name(api_name))
     cs.execute(l_sql)

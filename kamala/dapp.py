d7e164e3c7be4f8df6f9092c29e6944dbda4275f
Dmitry Mitrofanov <dmitry.mitrofanov@nordigy.ru>
2020-10-24 17:27:50 +0300

    bonuses work

--- kamala/dapp.py
+++ kamala/dapp.py
@@ -36,14 +36,14 @@ app.layout = html.Div([
             'textAlign': 'center',
             'margin': '10px'
         },
-        # Allow multiple files to be uploaded
+        # Dissalow multiple files to be uploaded
         multiple=False
     ),
     html.Div(id='output-upload-data'),
 ])
 
 
-def to_dataframe(file):
+def parse(file):
     data = pd.read_csv(file, sep=';', encoding='cp1251')
 
     lis = data.astype(str).values.tolist()
@@ -51,8 +51,6 @@ def to_dataframe(file):
     target = []
     for rows in lis:
         date = rows[1]
-        # if date:
-        # date = datetime.datetime.strptime(rows[1], '%Y-%m-%d %H:%M:%S')
         services = re.findall(r'([^;]+?, \d+,)', rows[3])
         l = map(lambda x: x.rstrip(',').strip(), services)
         for x in l:
@@ -61,45 +59,108 @@ def to_dataframe(file):
             t1 = x[:-i - 1].strip()
             t2 = x[-i:].strip()
             target.append([date, t1, int(t2)])
-    df = pd.DataFrame(target, columns=['DateTime', 'ServiceName', 'Count'])
-    df['DateTime'] = pd.to_datetime(df['DateTime'], format='%Y-%m-%d %H:%M:%S')
+    df = pd.DataFrame(target, columns=['day', 'service', 'count'])
+    #df['day'] = pd.to_datetime(df['day'], format='%Y-%m-%d').dt.floor('d')
+    df['day'] = pd.to_datetime(df['day'], format='%Y-%m-%d').dt.floor('d')
+    #df.day = pd.DatetimeIndex(df.day).strftime("%Y-%m-%d")
     return df
 
 
-def to_dash_table(df):
-    return dash_table.DataTable(
-        data=df.to_dict('records'),
-        columns=[{'name': i, 'id': i} for i in df.columns]
+def service_score():
+    df = pd.read_csv(
+        filepath_or_buffer='service_score.csv',
+        encoding='cp1251',
+        names=[
+            'service',
+            'score'
+        ]
     )
+    return df
 
 
-def editable_dash_table(df):
-    return dash_table.DataTable(
+def score_bonus():
+    df = pd.read_csv(
+        filepath_or_buffer='score_bonus.csv',
+        encoding='cp1251',
+        names=[
+            'score',
+            'bonus'
+        ]
+    )
+    return df
+
+
+def t(title, df):
+    tab = dash_table.DataTable(
         data=df.to_dict('records'),
         columns=[{'name': i, 'id': i} for i in df.columns],
-        editable=True,
+        export_format='csv'
     )
+    return html.Div([html.H3(title), tab])
 
 
 def groupby_sum(df, columns):
     return df.groupby(columns).sum().reset_index()
 
 
-def render_page(df):
-    raw_df = df
-    grouped = groupby_sum(
-        df, [pd.Grouper(key="DateTime", freq='D'), 'ServiceName'])
-    services = pd.unique(df['ServiceName'])
-    services = pd.DataFrame(data=services, columns=['ServiceName'])
-    return html.Div([
-        html.H3('Points per service'),
-        editable_dash_table(services),
-        html.H3('Grouped by service'),
-        to_dash_table(grouped),
-        html.H3('Raw Parsed Data'),
-        to_dash_table(raw_df),
+def render(df):
+    ra = df
+    se = service_score()
+    sc = score_bonus()
+
+    gr = groupby_sum(
+        ra,
+        [
+            pd.Grouper(
+                key="day",
+                freq='D'
+            ),
+            'service',
+        ])
+
+    #ct = pd.concat([gr, se], keys='service,score')
+    ct = pd.merge(gr, se, how='left', on='service')
+
+    ct['temp_score'] = ct['count'] * ct['score']
+
+    dt = ct.groupby('day').agg({'temp_score': ['sum']})
+    dt.columns = ['sum_temp_score']
+    dt = dt.reset_index()
+
+    dt = pd.merge(dt, sc, how='left',
+                  left_on='sum_temp_score', right_on='score')
+
+    total = dt.bonus.sum()
+
+    result = html.Div([
+        html.H2(f'Total bonus: {total}'),
+        t(
+            title='ref score bonus',
+            df=sc
+        ),
+        t(
+            title='ref service score',
+            df=se
+        ),
+        t(
+            title='day view',
+            df=dt
+        ),
+        t(
+            title='concated',
+            df=ct
+        ),
+        t(
+            title='grouped',
+            df=gr
+        ),
+        t(
+            title='raw',
+            df=ra
+        ),
     ])
-# editable=True
+
+    return result
 
 
 @ app.callback(Output('output-upload-data', 'children'),
@@ -107,21 +168,9 @@ def render_page(df):
 def update_output(contents, filename):
     if contents:
         decoded = base64.b64decode(contents).decode('cp1251')
-        df = to_dataframe(io.StringIO(decoded))
-        return render_page(df)
+        df = parse(io.StringIO(decoded))
+        return render(df)
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-"""
-with open('result20200912110233.csv', 'rb') as f:
-    ftext = f.read()
-    result = base64.b64encode(ftext)
-    decoded = base64.b64decode(result)
-    df = pd.read_csv(io.StringIO(decoded.decode('cp1251')))
-    dt = dash_table.DataTable(
-        data=df.to_dict('records'),
-        columns=[{'name': i, 'id': i} for i in df.columns]
-    )
-"""
